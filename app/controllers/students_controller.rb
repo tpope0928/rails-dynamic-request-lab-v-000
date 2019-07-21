e8de77bc121373a3c29a11aec5683d7636df146f
Thomas Pope <tpope0928@gmail.com>
2019-07-21 22:14:40 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,16 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
+  
   def index
     @students = Student.all
   end
 
   def show
-    @students = Student.find(params[:id])
   end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
